aac1203fcc2320704379f8b913050662b5bb2d8f
Lionel <phamngoctuong1805@gmail.com>
2020-02-06 05:02:52 +0700

    Create class Database

--- config/database.php
+++ config/database.php
@@ -1,1 +1,20 @@
-database.php
+<?php
+class Database
+{
+	private $conn;
+  private $servername = "localhost";
+  private $database   = "php_oop_crud_level_1";
+  private $username   = "root";
+  private $password   = "";
+  function __construct()
+	{
+	try {
+	  $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $e) {
+		  echo "Connection failed: " . $e->getMessage();
+		}		
+	}
+	return $this->conn;
+}
+?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php  
-	require_once 'layout_header.php';
-	require_once 'read_template.php';
-	require_once 'layout_footer.php';
+<?php
+require_once 'config/database.php';
+$database = new Database;
+require_once 'layout_header.php';
+require_once 'read_template.php';
+require_once 'layout_footer.php';
