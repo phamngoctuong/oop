36bd291e52bc26f811c70513d5a217839610103b
Lionel <phamngoctuong1805@gmail.com>
2020-02-06 23:53:22 +0700

    Creaate Product

--- objects/category.php
+++ objects/category.php
@@ -17,5 +17,12 @@
 	    $array = $stmt->fetch(PDO::FETCH_ASSOC);
 	    return $array['name'];
 		}
+		public function read()
+	  {
+	    $query = "SELECT id, name FROM " . $this->table . " ORDER BY name";
+	    $stmt  = $this->conn->prepare($query);
+	    $stmt->execute();
+	    return $stmt;
+	  }
 	}
 ?>
